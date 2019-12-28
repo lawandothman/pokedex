a3c7a379f1ca9e124a267e75c43a5ce9c6e5f48d
Lawand Othman <lawandothman@LAW.local>
2019-12-28 01:29:02 +0000

    added height

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const nunjucks = require("nunjucks");
 const axios = require("axios");
 const app = express();
 
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app
 });
 
 app.use(express.static("public"));
@@ -13,37 +13,30 @@ app.use(express.static("public"));
 app.get("/", function(request, response) {
   console.log("Someone is requesting the page /");
   response.render("index.html");
-
 });
 
 app.get("/pokemon/:id", async (req, res) => {
-  console.log("Someone is request a pokemon")
-  const id = req.params.id
-  const pokemon = await getPokemon(id)
+  console.log("Someone is request a pokemon");
+  const id = req.params.id;
+  const pokemon = await getPokemon(id);
   const moves = [];
-  for(move of pokemon.moves) {
-      moves.push(axios.get(move.move.url))
+  for (move of pokemon.moves) {
+    moves.push(axios.get(move.move.url));
   }
-  const resolvePromises = await Promise.all(moves)
-
-  console.log(resolvePromises)
+  const resolvePromises = await Promise.all(moves);
   res.render("pokemon.html", {
-    number: id, 
+    number: id,
     pokemon,
-    moves: resolvePromises,
-  })
-})
-
-// for moves :  type name 
-
+    moves: resolvePromises
+  });
+});
 
 async function getPokemon(id) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-  const res = await axios.get(url)
-  const pokemon = res.data
-  return pokemon
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await axios.get(url);
+  const pokemon = res.data;
+  return pokemon;
 }
-
 
 const listener = app.listen(process.env.PORT | 3000, function() {
   console.log("Your app is listening on port  " + listener.address().port);
