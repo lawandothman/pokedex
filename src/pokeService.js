8dee5d014839193f23cb6e13f36f28d8508468f7
Lawand Othman <lawandothman@yahoo.co.uk>
2020-02-11 21:22:06 +0000

    add pokeService

---
+++ src/pokeService.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+async function getPokemon(id) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await axios.get(url);
+  const pokemon = res.data;
+  return pokemon;
+}
+
+module.exports = { getPokemon };
